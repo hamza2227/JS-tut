11bca70c56ec0a9bc90aec8fd3349a7d62106502
Hamza Altaf <86216890+hamza2227@users.noreply.github.com>
2023-06-07 23:02:31 +0500

    js first commit

--- main.js
+++ main.js
@@ -149,9 +149,8 @@
 //     }
 // }
 
-for (let i = 0; i < 5; i++) {
-    console.log(".");
+for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-        console.log("*");
+        console.log("I "+i+" , J "+j);
     }
 }
